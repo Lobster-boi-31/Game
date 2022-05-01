f2a261e86c282328e7e1f50261d8ee76cf42e1e7
Lobster-boi-31 <73244261+Lobster-boi-31@users.noreply.github.com>
2022-05-01 10:11:03 -0400

    Update PlayerCollision.cs

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
 
-public PlayerMovment movement; 
+   public PlayerMovment movement; 
 
-void OnCollisionEnter (Collision collisioninfo)
-{
-   if (collisioninfo.collider.tag == "Obstacle")
-    {
-       movement.enabled = false;
-    }
-}
+   void OnCollisionEnter (Collision collisioninfo)
+   {
+       if (collisioninfo.collider.tag == "Obstacle")
+      {
+          movement.enabled = false;
+      }
+   }
 
 }
